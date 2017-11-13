2d768211beedd3d2d7360e24bcf96dce0c49a44d
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-13 00:02:43 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,12 +81,9 @@ def adding_matz
         }
      }
 
-		 # :yukihiro_matsumoto => {
-		 #   :known_for => "Ruby",
-		 #    :languages => ["LISP, C"]
-
+		
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash
 end
 
 def changing_alan
